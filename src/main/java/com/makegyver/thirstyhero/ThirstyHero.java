072ea440ed87921e7a6c1d891778c604b8567be0
Ben <9918452+Make-Gyver@users.noreply.github.com>
2024-10-04 21:50:29 +0200

    Initial commit

---
+++ src/main/java/com/makegyver/thirstyhero/ThirstyHero.java
@@ -0,0 +1,15 @@
+package com.makegyver.thirstyhero;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThirstyHero implements ModInitializer {
+	public static final String MOD_ID = "thirstyhero";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/makegyver/thirstyhero/ThirstyHeroClient.java
@@ -0,0 +1,10 @@
+package com.makegyver.thirstyhero;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThirstyHeroClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
